e4a2a4ac312166122410a56b58e662ee11656b25
dfder <a2470abc@gmail.com>
2022-03-09 22:35:27 +0800

    Initial commit

---
+++ src/main/java/tw/dfder/ccts/CctsApplication.java
@@ -0,0 +1,13 @@
+package tw.dfder.ccts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CctsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/dfder/ccts/CctsApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.dfder.ccts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
