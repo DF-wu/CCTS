4e898ae7bcf492d88633265458a7013d3f0d0e75
dfder <a2470abc@gmail.com>
2022-03-11 01:49:44 +0800

    feat: add some msg and modify code structure.

--- src/main/java/tw/dfder/ccts/services/StateParser.java
+++ src/main/java/tw/dfder/ccts/services/StateParser.java
@@ -2,20 +2,16 @@ package tw.dfder.ccts.services;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.Yaml;
 import tw.dfder.ccts.configuration.CCTSConfigure;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
 
 @Service
 public class StateParser {
-    InputStream CCTSFileStream;
 
     @Autowired
     public StateParser() {
@@ -25,13 +21,10 @@ public class StateParser {
     public Map<String, Object> parseYaml() {
         Yaml yaml = new Yaml();
         Map<String, Object> profileProperties = null;
-        Resource resource = new ClassPathResource("static/cctsProfile.yaml");
         try {
-            profileProperties  = yaml.load(resource.getInputStream());
-
+            profileProperties = yaml.load(CCTSConfigure.cctsFile.getInputStream());
         }catch (Exception e ){
             System.out.println(e);
-            System.out.println("!!!!!!!!!!!!");
         }
         return profileProperties;
     }
