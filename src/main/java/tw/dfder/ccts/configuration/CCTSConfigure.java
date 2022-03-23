e83a2f832d826b7ac6e4b29c1745bb85400db5d3
dfder <a2470abc@gmail.com>
2022-03-23 17:32:56 +0800

    fix: to inject Resource file in correct way.(non static).

--- src/main/java/tw/dfder/ccts/configuration/CCTSConfigure.java
+++ src/main/java/tw/dfder/ccts/configuration/CCTSConfigure.java
@@ -1,16 +1,14 @@
 package tw.dfder.ccts.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 @Configuration
 public class CCTSConfigure {
-    // not working
-//    @Value("classpath:static/cctsProfile.yaml")
-//    public static Resource cctsFile;
-
-    public static Resource cctsFile = new ClassPathResource("static/cctsProfile.yaml");
+    @Value("classpath:static/cctsProfile.yaml")
+    public Resource cctsFile;
 
 
 }
