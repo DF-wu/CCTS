cced16ee32309e906fd09b46164335bbaaecc12a
dfder <a2470abc@gmail.com>
2022-03-23 17:33:18 +0800

    fix: add some error messages.

--- src/main/java/tw/dfder/ccts/services/CCTSProfileParser.java
+++ src/main/java/tw/dfder/ccts/services/CCTSProfileParser.java
@@ -5,28 +5,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.Yaml;
 import tw.dfder.ccts.configuration.CCTSConfigure;
+import tw.dfder.ccts.entity.CCTSProfile;
 
-import java.io.InputStream;
 import java.util.Map;
 
 
 @Service
-public class StateParser {
+public class CCTSProfileParser {
+    private  CCTSConfigure cctsConfigure;
 
     @Autowired
-    public StateParser() {
+    public CCTSProfileParser(CCTSConfigure cctsConfigure) {
 
+        this.cctsConfigure = cctsConfigure;
     }
 
     public Map<String, Object> parseYaml() {
         Yaml yaml = new Yaml();
         Map<String, Object> profileProperties = null;
         try {
-            profileProperties = yaml.load(CCTSConfigure.cctsFile.getInputStream());
+            profileProperties = yaml.load(cctsConfigure.cctsFile.getInputStream());
         }catch (Exception e ){
+            System.out.println("CCTS profile not found or format not legal");
             System.out.println(e);
+
         }
         return profileProperties;
+
+    }
+
+    public CCTSProfile convert2CCTSProfile(Map<String, Object> yml){
+        CCTSProfile cctsFile = new CCTSProfile();
+
+        return cctsFile;
     }
 
 }
