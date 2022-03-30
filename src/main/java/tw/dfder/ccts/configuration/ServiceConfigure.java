8e8adbffa7b7da1f64ebdb9437e8e5b439622bc2
df <df@dfder.tw>
2022-03-31 02:43:33 +0800

    feat: change name.

--- src/main/java/tw/dfder/ccts/configuration/ServiceConfigure.java
+++ src/main/java/tw/dfder/ccts/configuration/ServiceConfigure.java
@@ -2,13 +2,14 @@ package tw.dfder.ccts.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 @Configuration
-public class CCTSConfigure {
+public class ServiceConfigure {
     @Value("classpath:static/cctsProfile.yaml")
     public Resource cctsFile;
 
+    @Value("${CCTS.pact_broker}")
+    public String pactBrokerUrlPrefix;
 
 }
