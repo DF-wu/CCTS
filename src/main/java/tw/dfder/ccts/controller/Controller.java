80f70f7ccf4641b76a084ac470960bef295f1c64
df <df@dfder.tw>
2022-04-04 02:34:00 +0800

    feat: add test case.

--- src/main/java/tw/dfder/ccts/controller/Controller.java
+++ src/main/java/tw/dfder/ccts/controller/Controller.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import tw.dfder.ccts.entity.CCTSProfile;
 import tw.dfder.ccts.services.CCTSProfileParser;
 import tw.dfder.ccts.services.PactBrokerBusyBox;
+import tw.dfder.ccts.services.StreamGobbler;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.concurrent.Executors;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -26,13 +28,25 @@ public class Controller {
     }
 
     @PostConstruct
-    public void testMethod() throws IOException {
+    public void testMethod() throws IOException, InterruptedException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         CCTSProfile cctsProfile = CCTSProfileParser.parse2CCTSProfile();
 
         System.out.println(CCTSProfileParser.findPathList(cctsProfile).size());
 
-        pactBrokerBusyBox.retrieveAllPacts();
+//        pactBrokerBusyBox.retrieveAllPacts();
+
+        Process process;
+
+
+
+        process = Runtime.getRuntime().exec("docker run --rm -e 23.dfder.tw:10141 pactfoundation/pact-cli:latest broker can-i-deploy --pacticipant loggingService --latest");
+        StreamGobbler streamGobbler =
+                new StreamGobbler(process.getInputStream(), System.out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler);
+        int exitCode = process.waitFor();
+        System.out.println(exitCode);
+
 
     }
 
