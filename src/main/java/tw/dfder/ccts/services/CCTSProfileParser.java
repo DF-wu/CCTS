3fd71891863281806b1846b6256f81ac86778e7d
df <df@dfder.tw>
2022-04-05 01:29:18 +0800

    refactor: naming convention.

--- src/main/java/tw/dfder/ccts/services/CCTSProfileParser.java
+++ src/main/java/tw/dfder/ccts/services/CCTSProfileParser.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 import tw.dfder.ccts.configuration.ServiceConfigure;
-import tw.dfder.ccts.entity.CCTSProfile;
-import tw.dfder.ccts.entity.NextState;
-import tw.dfder.ccts.entity.SimpleState;
+import tw.dfder.ccts.entity.CCTSModel.CCTSProfile;
+import tw.dfder.ccts.entity.CCTSModel.NextState;
+import tw.dfder.ccts.entity.CCTSModel.SimpleState;
 
 import java.util.ArrayList;
 import java.util.Map;
