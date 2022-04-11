0da361988cd0e7a65fa9e0249c9a95efcb7d9bae
df <df@dfder.tw>
2022-04-11 18:21:20 +0800

    feat: add java bean methods.

--- src/main/java/tw/dfder/ccts/entity/Contract.java
+++ src/main/java/tw/dfder/ccts/entity/Contract.java
@@ -5,6 +5,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.ArrayList;
+
 @Document(collection = "Contracts")
 public class Contract {
     @Id
@@ -16,9 +18,39 @@ public class Contract {
     @Field
     private String providerName;
 
+    @Field
+    private ArrayList<String> contractTerms;
 
 
+    public String getId() {
+        return Id;
+    }
 
+    public void setId(String id) {
+        Id = id;
+    }
 
+    public String getConsumerName() {
+        return consumerName;
+    }
 
+    public void setConsumerName(String consumerName) {
+        this.consumerName = consumerName;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
+    public ArrayList<String> getContractTerms() {
+        return contractTerms;
+    }
+
+    public void setContractTerms(ArrayList<String> contractTerms) {
+        this.contractTerms = contractTerms;
+    }
 }
