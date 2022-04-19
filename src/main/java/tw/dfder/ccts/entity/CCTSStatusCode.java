6d16f4a466e5c722fe0c302748d5b7894696280a
df <df@dfder.tw>
2022-04-19 22:31:24 +0800

    feat: add new error message.

--- src/main/java/tw/dfder/ccts/entity/CCTSStatusCode.java
+++ src/main/java/tw/dfder/ccts/entity/CCTSStatusCode.java
@@ -5,8 +5,8 @@ public enum CCTSStatusCode {
     ERROR_PROVIDER("provider not match CCTS document"),
     ERROR_CONSUMER("Consumer not match CCTS document"),
     ERROR_TESTCASEID("testCaseId not match CCTS document"),
-    PATH_TESTCASE_NOT_FOUND_IN_CONTRACT("the testCaseId is not found in contract");
-
+    PATH_TESTCASE_NOT_FOUND_IN_CONTRACT("the testCaseId is not found in contract"),
+    CONTREACT_TEST_RESULT_NOT_PASS("The service's contract test hasn't done fully");
     private final String infoMessage;
 
     CCTSStatusCode(String infoMessage) {
