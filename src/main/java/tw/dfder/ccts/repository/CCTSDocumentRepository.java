1cf61f84aaa2c96669e759facb1e078ca5b1c570
df <df@dfder.tw>
2022-04-20 23:31:22 +0800

    refactor: naming.

--- src/main/java/tw/dfder/ccts/repository/CCTSDocumentRepository.java
+++ src/main/java/tw/dfder/ccts/repository/CCTSDocumentRepository.java
@@ -3,7 +3,7 @@ package tw.dfder.ccts.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import tw.dfder.ccts.entity.CCTSModel.CCTSDocument;
+import tw.dfder.ccts.entity.cctsdocumentmodel.CCTSDocument;
 
 @Repository
 public interface CCTSDocumentRepository extends MongoRepository<CCTSDocument, String> {
