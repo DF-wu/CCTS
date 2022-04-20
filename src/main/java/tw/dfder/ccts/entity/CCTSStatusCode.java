395eb5d49f253d79dcc4bc4031879c96893b8b9f
df <df@dfder.tw>
2022-04-20 23:30:44 +0800

    feat: adjust some error status,

--- src/main/java/tw/dfder/ccts/entity/CCTSStatusCode.java
+++ src/main/java/tw/dfder/ccts/entity/CCTSStatusCode.java
@@ -2,9 +2,9 @@ package tw.dfder.ccts.entity;
 
 public enum CCTSStatusCode {
     ALLGREEN("All conditions passed."),
-    ERROR_PROVIDER("provider not match CCTS document"),
-    ERROR_CONSUMER("Consumer not match CCTS document"),
+    ERROR_PARTICIPANT("provider or consumer not match CCTS document"),
     ERROR_TESTCASEID("testCaseId not match CCTS document"),
+    ERROR_NO_EVENT_FOUND("There is no event between the provider and consumer"),
     PATH_TESTCASE_NOT_FOUND_IN_CONTRACT("the testCaseId is not found in contract"),
     CONTREACT_TEST_RESULT_NOT_PASS("The service's contract test hasn't done fully");
     private final String infoMessage;
