32cd49839c107b21be2a8df4d62f57b032584fd1
df <df@dfder.tw>
2022-04-20 23:36:20 +0800

    fix: refactor some function. more readable. enclosed result as a object.

--- src/main/java/tw/dfder/ccts/services/CCTSVerifier.java
+++ src/main/java/tw/dfder/ccts/services/CCTSVerifier.java
@@ -3,8 +3,9 @@ package tw.dfder.ccts.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tw.dfder.ccts.configuration.ServiceConfigure;
-import tw.dfder.ccts.entity.CCTSModel.CCTSDocument;
-import tw.dfder.ccts.entity.CCTSModel.NextState;
+import tw.dfder.ccts.entity.CCTSResult;
+import tw.dfder.ccts.entity.cctsdocumentmodel.CCTSDocument;
+import tw.dfder.ccts.entity.cctsdocumentmodel.NextState;
 import tw.dfder.ccts.entity.Contract;
 import tw.dfder.ccts.entity.CCTSStatusCode;
 import tw.dfder.ccts.entity.EventLog;
@@ -31,13 +32,16 @@ public class CCTSVerifier {
         this.serviceConfig = serviceConfig;
     }
 
-    public boolean verifyCCTSProfileSAGAFlow() {
+    public CCTSResult verifyCCTSProfileSAGAFlow() {
+
         boolean isSuccess = false;
 
         // retrieve needed data from db to memory for increasing speed
         ArrayList<CCTSDocument> documents = (ArrayList<CCTSDocument>) cctsDocumentRepository.findAll();
         ArrayList<EventLog> eventlogs = (ArrayList<EventLog>) eventLogRepository.findAll();
-        Hashtable<NextState, CCTSStatusCode> errorOccursMap = new Hashtable<>();
+        Hashtable<NextState, CCTSStatusCode> resultDict = new Hashtable<>();
+
+        CCTSResult cctsResult  = new CCTSResult(documents);
 
         for (CCTSDocument document : documents) {
             // specify a path as baseline.
@@ -51,42 +55,44 @@ public class CCTSVerifier {
                     }
                 }
 
-                // match path and eventlog
-                for (EventLog el : sameRouteEventlogs) {
-                    // if error ->  add into errors,
-                    // if no error -> passed
-                     CCTSStatusCode pathAndElResult = inspectErrors(path, el);
-                    if(pathAndElResult == CCTSStatusCode.ALLGREEN){
-                        // passed
-                    }else {
-                        // eventlog and path not match. add into errors map.
-                        errorOccursMap.put(path, inspectErrors(path, el));
-                    }
+                // if same route eventlogs not exist -> no related event was produced between the producer and consumer
+                if(sameRouteEventlogs.size() == 0 ){
+                    cctsResult.getResultBetweenPathAndEventLogs().put(path, CCTSStatusCode.ERROR_NO_EVENT_FOUND);
+                    // jump to next path
+                    continue;
                 }
 
-                // match path and contract which is from pact broker
-                CCTSStatusCode pathAndContractResult = verifyPathAndContract(path);
-                if(pathAndContractResult == CCTSStatusCode.ALLGREEN){
-                    // passed
-                }else {
-                    // add into error map
-                    errorOccursMap.put(path, pathAndContractResult);
-                }
+                // verify path and eventlogs and get error code if exist
+                // add errors to result
+                cctsResult.getResultBetweenPathAndEventLogs().putAll(verifyPathAndEventlog(path, sameRouteEventlogs));
+
+                // match path and corresponded contract testCaseId
+                cctsResult.getResultBetweenPathAndContract().put(path, verifyPathAndContract(path));
             }
 
-
-            // check contract test status
-            isSuccess =  errorTellerContractTestError(validateServiceContractTestResult(document));
-
-
-            // this document's error
-            isSuccess = errorTellerPathMathError(errorOccursMap);
-
+            // check contract verification status
+            cctsResult.getContractVerificationErrors().putAll(validateServiceContractTestResult(document));
 
         }
-        return isSuccess;
+        return cctsResult;
     }
 
+    private Hashtable<NextState, CCTSStatusCode> verifyPathAndEventlog (NextState path, ArrayList<EventLog> sameRouteEventlogs) {
+        Hashtable<NextState, CCTSStatusCode> errorsDict = new Hashtable<>();
+        // match path and eventlog
+        for (EventLog el : sameRouteEventlogs) {
+            // if error ->  add into errors,
+            // if no error -> passed
+             CCTSStatusCode pathAndElResult = inspectPathAndEventLog(path, el);
+            if(pathAndElResult == CCTSStatusCode.ALLGREEN){
+                // pass
+            }else {
+                // eventlog and path not match. add into errors map.
+                errorsDict.put(path, pathAndElResult);
+            }
+        }
+        return errorsDict;
+    }
 
 
     //for inspect contract test result error
@@ -141,7 +147,7 @@ public class CCTSVerifier {
         return isValidPath ? CCTSStatusCode.ALLGREEN : CCTSStatusCode.PATH_TESTCASE_NOT_FOUND_IN_CONTRACT;
     }
 
-    private CCTSStatusCode inspectErrors(NextState path, EventLog el){
+    private CCTSStatusCode inspectPathAndEventLog(NextState path, EventLog el){
         if (path.getProvider().equals(el.getProviderName())) {
             // provider match
             if (path.getConsumer().equals(el.getConsumerName())) {
@@ -156,11 +162,11 @@ public class CCTSVerifier {
                 }
             } else {
                 //consumer not match
-                return CCTSStatusCode.ERROR_CONSUMER;
+                return CCTSStatusCode.ERROR_PARTICIPANT;
             }
         } else {
             // provider not match
-            return CCTSStatusCode.ERROR_PROVIDER;
+            return CCTSStatusCode.ERROR_PARTICIPANT;
         }
 
     }
@@ -173,6 +179,7 @@ public class CCTSVerifier {
             participantsServices.add(state.getConsumer());
         }
 
+        // create an errors Dict
         HashMap<String,  CCTSStatusCode> errors = new HashMap<String, CCTSStatusCode>();
         HashMap<String, Boolean> contractTestresults = fetchContractTestResultConcurrently(participantsServices);
         for (String service: contractTestresults.keySet()) {
