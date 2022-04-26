15f79e4f8dd439869cf423d18a5b701f4c74eea3
df <df@dfder.tw>
2022-04-27 00:16:26 +0800

    feat: update CCTS document to 0.9.

--- src/main/java/tw/dfder/ccts/entity/CCTSStatusCode.java
+++ src/main/java/tw/dfder/ccts/entity/CCTSStatusCode.java
@@ -1,14 +1,25 @@
 package tw.dfder.ccts.entity;
 
+import tw.dfder.ccts.entity.cctsdocumentmodel.CCTSDocument;
+
 public enum CCTSStatusCode {
     ALLGREEN("All conditions passed."),
     ERROR_PARTICIPANT("provider or consumer not match CCTS document"),
     ERROR_TESTCASEID_IN_CONTRACT("Contract testCaseId not match CCTS document"),
-
     ERROR_NO_MATCH_TESTCASEID_IN_EVENTLOGS("There is no correspond testCaseId in eventlog for this delivery."),
     ERROR_NO_EVENT_FOUND("There is no event between the provider and consumer"),
     DELIVERY_TESTCASEID_NOT_FOUND_IN_CONTRACT("the testCaseId is not found in contract"),
-    CONTREACT_TEST_RESULT_NOT_PASS("The service's contract test hasn't done fully");
+    CONTREACT_TEST_RESULT_NOT_PASS("The service's contract test hasn't done fully"),
+
+    // parser error
+    CCTSDOCUMENT_ERROR("Some properties cause error while parsing CCTS document");
+
+
+
+
+
+
+
     private final String message;
 
     CCTSStatusCode(String message) {
