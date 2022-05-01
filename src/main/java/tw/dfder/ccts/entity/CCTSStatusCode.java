94caa517c0f5fa0c9b12d65852c1f0527a6dd6bb
df <df@dfder.tw>
2022-05-02 01:48:32 +0800

    feat: TODO update to new CCTS verify process.

--- src/main/java/tw/dfder/ccts/entity/CCTSStatusCode.java
+++ src/main/java/tw/dfder/ccts/entity/CCTSStatusCode.java
@@ -10,16 +10,18 @@ public enum CCTSStatusCode {
     CONTREACT_TEST_RESULT_NOT_PASS("The service's contract test hasn't done fully"),
 
     // parser error
-    CCTSDOCUMENT_ERROR("Some properties cause error while parsing CCTS document"),
+    CCTSDOCUMENT_REQUIRED_PROPERTIES_NULL_ERROR("Some properties cause error while parsing CCTS document"),
     CCTSDOCUMENT_ERROR_STATENAME_NOT_FOUND("some state name may not correspond to CCTS document"),
-    CCTSDOCUMENT_ERROR_CASESEQUENCE_NOT_LEGAL("Case sequence is not legal"),
+    CCTSDOCUMENT_PARSE_ERROR("unknown error occur while parsing CCTS document"),
+    CCTSDOCUMENT_DUPLICATED_TITLE_ERROR("Some documents title is duplicated"),
+    CCTSDOCUMENT_ERROR_STATE_NOT_REACHABLE("some state is not reachable"),
 
     PATH_SEQUENCE_ERROR("Eventlog produced time are not qualified the sequence"),
 
     PATH_NOT_CONNECTED("nextstate consumer is not connected by previous state"),
 
-    PATH_NOT_EVENTLOG_FOUND("There is no corresponded eventlog found for this path");
-
+    PATH_NOT_EVENTLOG_FOUND("There is no corresponded eventlog found for this path"),
+    DOCUMENT_DUPLICATED_STATE_NAME_ERROR("There are duplicated state name in CCTS document");
 
 
 
