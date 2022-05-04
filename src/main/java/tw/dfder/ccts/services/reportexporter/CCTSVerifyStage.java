343d9d3701320a8859615f4b1b72b55514b6d113
df <df@dfder.tw>
2022-05-04 23:25:31 +0800

    fix: move to other location for testing.

---
+++ src/main/java/tw/dfder/ccts/services/reportexporter/CCTSVerifyStage.java
@@ -0,0 +1,14 @@
+package tw.dfder.ccts.services.reportexporter;
+
+
+import tw.dfder.ccts.entity.cctsresultmodel.CCTSTest;
+
+public class CCTSVerifyStage implements ReportExportEngine{
+
+
+    @Override
+    public String exportReport(CCTSTest cctsTest) {
+        return null;
+    }
+}
+
