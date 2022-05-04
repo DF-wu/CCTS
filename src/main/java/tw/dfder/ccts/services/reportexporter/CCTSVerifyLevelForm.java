05e5ca6b7f12543762fcec3acb4f9acd37865d2c
df <df@dfder.tw>
2022-05-05 00:16:02 +0800

    feat: add CCTS Test exporter enging.

---
+++ src/main/java/tw/dfder/ccts/services/reportexporter/CCTSVerifyLevelForm.java
@@ -0,0 +1,10 @@
+package tw.dfder.ccts.services.reportexporter;
+
+import tw.dfder.ccts.entity.cctsresultmodel.CCTSTest;
+
+public class CCTSVerifyLevelForm implements ReportExportEngine {
+    @Override
+    public String exportReport(CCTSTest cctsTest) {
+        return null;
+    }
+}
