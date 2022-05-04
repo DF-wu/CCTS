90df57b2d0eb9342daec24118bf000674af8482e
df <df@dfder.tw>
2022-05-05 01:43:51 +0800

    feat: add CCTS Test result of passing or not.

--- src/main/java/tw/dfder/ccts/entity/cctsresultmodel/CCTSTest.java
+++ src/main/java/tw/dfder/ccts/entity/cctsresultmodel/CCTSTest.java
@@ -11,11 +11,8 @@ import java.util.Map;
 
 @Document(collection = "CCTSTest")
 public class CCTSTest {
-
+    private boolean cctsTestResult = false;
     private ArrayList<CCTSResult> results;
-
-
-
 
     public CCTSTest(ArrayList<CCTSDocument> cctsDocuments) {
         this.results = new ArrayList<>();
@@ -24,12 +21,32 @@ public class CCTSTest {
         }
     }
 
+    public boolean checkOut() {
+        boolean isAllPassed = true;
+        for ( CCTSResult result : results ) {
+            if ( result.isTestResult() ) {
+                // pass
+            }else {
+                // any fail
+                isAllPassed = false;
+            }
+        }
+        cctsTestResult = isAllPassed;
+        return cctsTestResult;
+    }
+
+
+
     public ArrayList<CCTSResult> getResults() {
         return results;
     }
 
     public void setResults(ArrayList<CCTSResult> results) {
         this.results = results;
+    }
+
+    public boolean isCctsTestResult() {
+        return cctsTestResult;
     }
 
 }
