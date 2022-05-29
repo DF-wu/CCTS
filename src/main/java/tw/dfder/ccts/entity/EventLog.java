24e6b9b7afc21c65302ae81871b100f54c093f6f
df <df@dfder.tw>
2022-05-29 23:36:43 +0800

    feat: change to normal db. for test.

--- src/main/java/tw/dfder/ccts/entity/EventLog.java
+++ src/main/java/tw/dfder/ccts/entity/EventLog.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection = "EventLogs_timeFailure")
+@Document(collection = "EventLogs")
 public class EventLog {
     @Id
     private String Id;
