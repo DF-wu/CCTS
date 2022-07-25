83ac8b0155288cc07b56271daff5c7c5d58e36e3
dfder <a2470abc@gmail.com>
2022-07-25 16:40:29 +0800

    feat: add docker compose run.

--- build.sh
+++ build.sh
@@ -5,14 +5,22 @@
 # by stanley2058@yahoo.com.tw
 # 所以這是先在外面build ，再把build好的檔案 copy到working dir 再build
 
+echo "build.sh : start maven build"
 mvn -T 1C clean install -Dmaven.test.skip=true
 echo "build.sh : maven build successfully"
 
+echo "build.sh : start to copy built files"
 cp ./target/CCTS-0.0.1.jar app.jar
 echo "build.sh : cp apps from target directory to root directory"
 
+
+echo "build.sh : building docker image"
 docker build . -t ccts
 echo "build.sh : docker build successfully"
+
+echo "build.sh : start to run CCTS project"
+
+docker compose up -d
 
 
 
