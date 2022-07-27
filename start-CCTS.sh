9dfb7865f53f178e8fc230da1fdfc43e595a8ff9
dfder <a2470abc@gmail.com>
2022-07-27 16:40:21 +0800

    feat: finish script for deployment

--- start-CCTS.sh
+++ start-CCTS.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+
+echo "stop and remove exist container."
+sh docker compose down
+
+echo "stop and remove exist container. Done."
+
 echo "start to build."
 sh ./build.sh
+
+echo "start container."
+sh docker compose up -d
+echo "Container is up now !"
